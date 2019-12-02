d8144a3bad6edcac933f998914c209546f99f736
dkchacha <dk.cho1996@gmail.com>
2019-12-02 20:23:09 +1300

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,26 @@
+## Storing the data
+powerConsumption <- read.table("C:/Users/dkcho/Downloads/exdata_data_household_power_consumption/household_power_consumption.txt", skip=1, sep=";")
+
+## Giving heading names to the data
+names(powerConsumption) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+## Getting the relevant data dates and storing it
+febPowerConsumption <- subset(powerConsumption, powerConsumption$Date == "1/2/2007" | powerConsumption$Date == "2/2/2007")
+
+## Require the data to be converted from characters to date/POSIX1t
+febPowerConsumption$Date <- as.Date(febPowerConsumption$Date, format="%d/%m/%Y")
+febPowerConsumption$Time <- strptime(febPowerConsumption$Time, format="%H:%M:%S")
+
+## Need to divide time into minutes
+febPowerConsumption[1:1440,"Time"] <- format(febPowerConsumption[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+febPowerConsumption[1441:2880,"Time"] <- format(febPowerConsumption[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+## Plotting the Graph
+plot(febPowerConsumption$Time, febPowerConsumption$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+
+## Adding the points
+points(febPowerConsumption$Time, febPowerConsumption$Sub_metering_2, type = "l", col = "red")
+points(febPowerConsumption$Time, febPowerConsumption$Sub_metering_3, type = "l", col = "blue")
+
+## Adding the key for the lines
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
