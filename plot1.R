fda0e8ceb7ae36840b197b79ecd43d77eebc2479
dkchacha <dk.cho1996@gmail.com>
2019-12-02 19:58:55 +1300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ powerConsumption <- read.table("C:/Users/dkcho/Downloads/exdata_data_household_p
 names(powerConsumption) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 
 ## Getting the relevant data dates and storing it
-febPowerConsumption <- subset(powerConsumption, powerConsumption$Date == "1/2/2007" | power$Date == "2/2/2007")
+febPowerConsumption <- subset(powerConsumption, powerConsumption$Date == "1/2/2007" | powerConsumption$Date == "2/2/2007")
 
 ## Plotting the graph
 hist(as.numeric(as.character(febPowerConsumption$Global_active_power)), col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
