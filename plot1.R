b6611e325c54ac66c4a0d9916f267e40b2c6deff
dkchacha <dk.cho1996@gmail.com>
2019-12-03 15:46:15 +1300

    Update plot1.R
    Getting rid of identifiers and specifying plot

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 ## Storing the data
-powerConsumption <- read.table("C:/Users/dkcho/Downloads/exdata_data_household_power_consumption/household_power_consumption.txt", skip=1, sep=";")
+powerConsumption <- read.table("household_power_consumption.txt", skip=1, sep=";")
 
 ## Giving heading names to the data
 names(powerConsumption) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
@@ -7,8 +7,14 @@ names(powerConsumption) <- c("Date", "Time", "Global_active_power", "Global_reac
 ## Getting the relevant data dates and storing it
 febPowerConsumption <- subset(powerConsumption, powerConsumption$Date == "1/2/2007" | powerConsumption$Date == "2/2/2007")
 
+# Determining the copy file format and its size
+png("plot1.png", width = 480, height = 480)
+
 ## Plotting the graph
 hist(as.numeric(as.character(febPowerConsumption$Global_active_power)), col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
 
 ## Plastering the title on
 title(main = "Global Active Power")
+
+# dev.off
+dev.off()
